1bb1903d15a859855309b43d6d2d864f295c56c9
ltj54 <latajo@gmail.com>
2025-12-03 15:55:23 +0100

    Auto-commit (feature/Faa-orden-paa-databasen) - 2025-12-03 15:55

--- backend/src/main/java/io/ltj/restructuring/config/SecurityConfig.java
+++ backend/src/main/java/io/ltj/restructuring/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
-                        // PUBLIC ENDPOINTS ----------------------
+                        // PUBLIC ENDPOINTS ---------------------
                         .requestMatchers(
                                 "/api/hello",
                                 "/api/config",
